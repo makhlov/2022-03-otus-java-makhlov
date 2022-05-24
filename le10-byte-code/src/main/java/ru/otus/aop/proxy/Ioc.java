089b1728a156e4db3f8452a446cb07e7398b3be0
Makhlov Egor <71174890+makhlov@users.noreply.github.com>
2022-05-24 23:46:56 +0300

    le10-byte-code comments added

--- le10-byte-code/src/main/java/ru/otus/aop/proxy/Ioc.java
+++ le10-byte-code/src/main/java/ru/otus/aop/proxy/Ioc.java
@@ -9,7 +9,9 @@ class Ioc {
     private Ioc() {
     }
 
+    //Логирование добавляется внешним арбитром через Dynamic Proxy (штука довольно старая)
     static MyClassInterface createMyClass() {
+        // 1. Создается экземпляр оригинального класса и помещается в специальный handler
         InvocationHandler handler = new DemoInvocationHandler(new MyClassImpl());
         return (MyClassInterface) Proxy.newProxyInstance(Ioc.class.getClassLoader(),
                 new Class<?>[]{MyClassInterface.class}, handler);
@@ -24,8 +26,12 @@ class Ioc {
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            //После того как handler заряжается нашим классом, сперва выполняется метод логирования
+            //А затем мы выполняем метод оригинального класса
             System.out.println("invoking method:" + method);
             return method.invoke(myClass, args);
+            //Этот метод передается в Proxy и мы с этой Proxy уже работаем
+
         }
 
         @Override
--- le10-byte-code/src/main/java/ru/otus/aop/proxy/ProxyDemo.java
+++ le10-byte-code/src/main/java/ru/otus/aop/proxy/ProxyDemo.java
@@ -2,8 +2,15 @@ package ru.otus.aop.proxy;
 
 public class ProxyDemo {
     public static void main(String[] args) {
+        // (1) Нам нужен оригинальный класс (экземпляр)
+        // (2) Нам необходимо перехватить все вызовы к классу, для этого создается Proxy, который стоит в середине и
+        // перехватывает все вызовы к оригинальному классу.
+
+
+        //Мы работаем не с оригинальным классом, а с его оберткой
         MyClassInterface myClass = Ioc.createMyClass();
-        myClass.secureAccess("Security Param");
+        myClass.secureAccess("Security Param"); //Когда мы обращаемся к instance этой штуки мы сперва вызываем
+        //метод прокси, а затем принимаем решение, вызываем мы оригинальный метод или нет
     }
 }
 
