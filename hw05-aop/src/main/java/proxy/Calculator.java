1c64df849c5d3fcce97adf64efba1556ba2b4833
Makhlov Egor <71174890+makhlov@users.noreply.github.com>
2022-05-25 21:27:46 +0300

    hw05-aop removed from main

--- hw05-aop/src/main/java/proxy/Calculator.java
+++
@@ -1,7 +0,0 @@
-package proxy;
-
-public interface Calculator {
-    void calculation(int param1);
-    void calculation(int param1, int param2);
-    void calculation(int param1, int param2, String param3);
-}
