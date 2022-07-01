48f0bb49237a4c980b766b648e3fe2e9a2375a75
Makhlov Egor <71174890+makhlov@users.noreply.github.com>
2022-07-01 13:57:35 +0300

    hw09-jdbc initial

---
+++ hw09-jdbc/src/main/java/ru/otus/necessary/crm/service/DBServiceManager.java
@@ -0,0 +1,15 @@
+package ru.otus.necessary.crm.service;
+
+import ru.otus.necessary.crm.model.Manager;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DBServiceManager {
+
+    Manager saveManager(Manager client);
+
+    Optional<Manager> getManager(long no);
+
+    List<Manager> findAll();
+}
