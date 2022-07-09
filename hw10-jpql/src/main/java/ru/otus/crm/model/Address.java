66e37404dde3bebeaff53096e10bd8de8c9fd75c
Makhlov Egor <71174890+makhlov@users.noreply.github.com>
2022-07-09 22:54:06 +0300

    hw10-jpql completed (#11)

---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Address.java
@@ -0,0 +1,37 @@
+package ru.otus.crm.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address implements Cloneable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    public Address() {
+    }
+
+    public Address(Long id, String street) {
+        this.id = id;
+        this.street = street;
+    }
+
+    @Override
+    public Address clone() {
+        return new Address(
+                this.id,
+                this.street
+        );
+    }
+}
